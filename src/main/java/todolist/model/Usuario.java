df970030aa7947c83a14df1912b525bf8d244738
Nikola Jamić <nikola.jamic@fer.hr>
2026-05-05 18:40:55 +0200

    Fourth test - Adding many-to-many relationship bug fixes

--- src/main/java/todolist/model/Usuario.java
+++ src/main/java/todolist/model/Usuario.java
@@ -24,6 +24,10 @@ public class Usuario implements Serializable {
     @Column(name = "fecha_nacimiento")
     @Temporal(TemporalType.DATE)
     private Date fechaNacimiento;
+    @Column(name = "is_admin")
+    private Boolean isAdmin = false;
+    @Column(name = "is_blocked")
+    private Boolean isBlocked = false;
 
     // La relación es lazy por defecto,
     // es necesario acceder a la lista de tareas para que se carguen
@@ -82,6 +86,22 @@ public class Usuario implements Serializable {
 
     public void setFechaNacimiento(Date fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public Boolean getIsBlocked() {
+        return isBlocked;
+    }
+
+    public void setIsBlocked(Boolean isBlocked) {
+        this.isBlocked = isBlocked;
     }
 
     // Getters y setters de la relación
